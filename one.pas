18537aa4880422ee294a37bd1d622d5bd9969f5a
l1me <102348214+dev-lime@users.noreply.github.com>
2024-11-12 14:19:06 +0500

    a new program

--- one.pas
+++ one.pas
@@ -1,4 +1,4 @@
-program DatabaseFromFile;
+program DynamicDatabase;
 
 uses SysUtils;
 
@@ -14,96 +14,84 @@ type
 var
   People: array of TPerson;
 
-procedure LoadDataFromFile(const FileName: string);
+procedure AddPerson;
 var
-  F: Text;
-  Line: string;
-  CurrentPerson: TPerson;
+  Person: TPerson;
   ChildID: string;
 begin
-  Assign(F, FileName);  // Исправлено AssignFile на Assign
-  Reset(F);
-  SetLength(People, 0);
+  WriteLn('Введите данные для нового человека (двойной Enter для завершения ввода):');
 
-  while not Eof(F) do
+  // Ввод ФИО
+  Write('ФИО: ');
+  ReadLn(Person.FullName);
+  if Person.FullName = '' then Exit;
+
+  // Ввод пола
+  Write('Пол: ');
+  ReadLn(Person.Gender);
+
+  // Ввод даты рождения
+  Write('Дата рождения: ');
+  ReadLn(Person.BirthDate);
+
+  // Ввод уникального ID
+  Write('Уникальный ID: ');
+  ReadLn(Person.IDNumber);
+
+  // Ввод ID детей
+  WriteLn('ID детей (через пробел, пустой ввод завершает список детей):');
+  SetLength(Person.ChildrenIDs, 0);
+  while True do
   begin
-    Readln(F, Line);
-    
-    if Line <> '' then
-    begin
-      // Чтение ФИО
-      CurrentPerson.FullName := Line;
-      
-      // Чтение пола
-      Readln(F, Line);
-      CurrentPerson.Gender := Line;
-      
-      // Чтение даты рождения
-      Readln(F, Line);
-      CurrentPerson.BirthDate := Line;
-      
-      // Чтение номера удостоверения личности
-      Readln(F, Line);
-      CurrentPerson.IDNumber := Line;
-
-      // Чтение номеров детей
-      SetLength(CurrentPerson.ChildrenIDs, 0);
-      while not Eof(F) do
-      begin
-        Readln(F, Line);
-        if Line = '---' then Break;
-
-        SetLength(CurrentPerson.ChildrenIDs, Length(CurrentPerson.ChildrenIDs) + 1);
-        CurrentPerson.ChildrenIDs[High(CurrentPerson.ChildrenIDs)] := Line;
-      end;
-
-      SetLength(People, Length(People) + 1);
-      People[High(People)] := CurrentPerson;
-    end;
+    ReadLn(ChildID);
+    if ChildID = '' then Break;
+    SetLength(Person.ChildrenIDs, Length(Person.ChildrenIDs) + 1);
+    Person.ChildrenIDs[High(Person.ChildrenIDs)] := ChildID;
   end;
-  
-  Close(F);  // Исправлено CloseFile на Close
-end;
 
-function FindPersonByID(const ID: string): string;
-var
-  i: Integer;
-begin
-  FindPersonByID := '- - -';  // Задаем начальное значение Result
-  for i := 0 to High(People) do
-    if People[i].IDNumber = ID then
-    begin
-      FindPersonByID := People[i].FullName;
-      Exit;
-    end;
+  // Добавляем человека в базу данных
+  SetLength(People, Length(People) + 1);
+  People[High(People)] := Person;
 end;
 
 procedure PrintDatabase;
 var
   i, j: Integer;
-  ChildName: string;
 begin
   for i := 0 to High(People) do
   begin
     WriteLn('ФИО: ', People[i].FullName);
     WriteLn('Пол: ', People[i].Gender);
     WriteLn('Дата рождения: ', People[i].BirthDate);
-    WriteLn('Номер удостоверения личности: ', People[i].IDNumber);
-    WriteLn('Дети:');
+    WriteLn('Уникальный ID: ', People[i].IDNumber);
+    Write('Дети: ');
     
     if Length(People[i].ChildrenIDs) = 0 then
-      WriteLn('  Нет детей')
+      WriteLn('Нет детей')
     else
       for j := 0 to High(People[i].ChildrenIDs) do
-      begin
-        ChildName := FindPersonByID(People[i].ChildrenIDs[j]);
-        WriteLn('  Номер удостоверения: ', People[i].ChildrenIDs[j], ' ФИО: ', ChildName);
-      end;
+        Write(People[i].ChildrenIDs[j], ' ');
+        
+    WriteLn;
     WriteLn;
   end;
 end;
 
+var
+  Input: string;
+
 begin
-  LoadDataFromFile('data.txt');
+  WriteLn('Начинайте ввод данных. Для завершения ввода нажмите два раза Enter.');
+  repeat
+    AddPerson;
+    WriteLn;
+    
+    // Проверка на двойной Enter
+    Write('Нажмите Enter для продолжения или снова Enter для завершения: ');
+    ReadLn(Input);
+  until Input = '';
+
+  WriteLn;
+  WriteLn('Все данные:');
   PrintDatabase;
 end.
