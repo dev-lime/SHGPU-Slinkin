c2792b9a55ec0305f926fd0e069fb836e316ca61
l1me <102348214+dev-lime@users.noreply.github.com>
2024-12-09 14:09:49 +0500

    added lab2 solution

--- programming/lab2/lab2.pas
+++ programming/lab2/lab2.pas
@@ -1,3 +1,5 @@
+uses math;
+
 type
   TSingleSet = set of byte;
   TLongSet = array of TSingleSet;
@@ -7,9 +9,10 @@ function createSet(count: integer): TLongSet;
 var
   setSize: integer;
 begin
+  createSet := nil;
   { Размер множества всегда кратен 256 }
-  setSize := ((count + 255) div 256) * 256;
-  SetLength(createSet, setSize div 256);
+  setSize := ((count + 255) div 256);
+  SetLength(createSet, setSize);
 end;
 
 { Функция изменения размера множества }
@@ -18,8 +21,8 @@ var
   setSize: integer;
 begin
   { Размер множества всегда кратен 256 }
-  setSize := ((newCount + 255) div 256) * 256;
-  SetLength(setSize, setSize div 256);
+  setSize := ((newCount + 255) div 256);
+  SetLength(dstSet, setSize);
 end;
 
 { Функция получения размера множества }
@@ -41,7 +44,10 @@ var
 begin
   index := e div 256;
   bitPos := e mod 256;
-  inSet := (bSet[index] and (1 shl bitPos)) <> 0;
+  if index < Length(bSet) then
+    inSet := byte(bitPos) in bSet[index]
+  else
+    inSet := False;
 end;
 
 { Аналог операции + (объединение), возвращает новое множество }
@@ -49,10 +55,16 @@ function sumSet(set1, set2: TLongSet): TLongSet;
 var
   i, len: integer;
 begin
+  sumSet := nil;
   len := Max(Length(set1), Length(set2));
-  SetLength(Result, len);
+  SetLength(sumSet, len);
   for i := 0 to len - 1 do
-    sumSet[i] := set1[i] or set2[i];
+  begin
+    if i < Length(set1) then
+      sumSet[i] := set1[i];
+    if i < Length(set2) then
+      sumSet[i] := sumSet[i] + set2[i];
+  end;
 end;
 
 { Аналог операции - (разность), возвращает новое множество }
@@ -60,10 +72,16 @@ function subSet(set1, set2: TLongSet): TLongSet;
 var
   i, len: integer;
 begin
+  subSet := nil;
   len := Max(Length(set1), Length(set2));
-  SetLength(Result, len);
+  SetLength(subSet, len);
   for i := 0 to len - 1 do
-    Result[i] := set1[i] and not set2[i];
+  begin
+    if i < Length(set1) then
+      subSet[i] := set1[i];
+    if i < Length(set2) then
+      subSet[i] := subSet[i] - set2[i];
+  end;
 end;
 
 { Аналог операции * (пересечение), возвращает новое множество }
@@ -71,10 +89,14 @@ function mulSet(set1, set2: TLongSet): TLongSet;
 var
   i, len: integer;
 begin
+  mulSet := nil;
   len := Max(Length(set1), Length(set2));
-  SetLength(Result, len);
+  SetLength(mulSet, len);
   for i := 0 to len - 1 do
-    mulSet[i] := set1[i] and set2[i];
+  begin
+    if (i < Length(set1)) and (i < Length(set2)) then
+      mulSet[i] := set1[i] * set2[i];
+  end;
 end;
 
 { Аналог функции include (добавление элемента) }
@@ -84,7 +106,9 @@ var
 begin
   index := e div 256;
   bitPos := e mod 256;
-  dstSet[index] := dstSet[index] or (1 shl bitPos);
+  if index >= Length(dstSet) then
+    SetLength(dstSet, index + 1);
+  Include(dstSet[index], byte(bitPos));
 end;
 
 { Аналог функции exclude (удаление элемента) }
@@ -94,7 +118,8 @@ var
 begin
   index := e div 256;
   bitPos := e mod 256;
-  dstSet[index] := dstSet[index] and not (1 shl bitPos);
+  if index < Length(dstSet) then
+    Exclude(dstSet[index], byte(bitPos));
 end;
 
 { Симметричная разность (объединение минус пересечение) }
@@ -102,8 +127,92 @@ function symDiffSet(set1, set2: TLongSet): TLongSet;
 var
   i, len: integer;
 begin
+  symDiffSet := nil;
   len := Max(Length(set1), Length(set2));
-  SetLength(Result, len);
+  SetLength(symDiffSet, len);
   for i := 0 to len - 1 do
-    symDiffSet[i] := set1[i] xor set2[i];
+  begin
+    if (i < Length(set1)) and (i < Length(set2)) then
+      symDiffSet[i] := (set1[i] + set2[i]) - (set1[i] * set2[i])
+    else if i < Length(set1) then
+      symDiffSet[i] := set1[i]
+    else
+      symDiffSet[i] := set2[i];
+  end;
 end;
+
+var
+  set1, set2, resultSet: TLongSet;
+  i: integer;
+
+begin
+  { Создание множества }
+  set1 := createSet(300); { Создаёт множество, способное содержать элементы от 0 до 299 }
+
+  { Добавление элементов }
+  includeSet(set1, 5);    { Добавляем элемент 5 }
+  includeSet(set1, 150);  { Добавляем элемент 150 }
+  includeSet(set1, 290);  { Добавляем элемент 290 }
+
+  { Проверка наличия элемента }
+  if inSet(set1, 150) then
+    writeln('Элемент 150 есть в множестве set1.')
+  else
+    writeln('Элемента 150 нет в множестве set1.');
+
+  if inSet(set1, 10) then
+    writeln('Элемент 10 есть в множестве set1.')
+  else
+    writeln('Элемента 10 нет в множестве set1.');
+
+  { Удаление элемента }
+  excludeSet(set1, 150); { Удаляем элемент 150 }
+  if not inSet(set1, 150) then
+    writeln('Элемент 150 успешно удалён из множества set1.');
+
+  { Изменение размера множества }
+  setSize(set1, 500); { Изменяем размер множества на 500 элементов }
+
+  { Создание второго множества и объединение }
+  set2 := createSet(300);
+  includeSet(set2, 290);
+  includeSet(set2, 100);
+  resultSet := sumSet(set1, set2); { Объединение set1 и set2 }
+
+  writeln('Элементы множества после объединения:');
+  for i := 0 to 500 do
+    if inSet(resultSet, i) then
+      write(i, ' ');
+  writeln;
+
+  { Пересечение множеств }
+  resultSet := mulSet(set1, set2); { Пересечение set1 и set2 }
+  writeln('Элементы множества после пересечения:');
+  for i := 0 to 500 do
+    if inSet(resultSet, i) then
+      write(i, ' ');
+  writeln;
+
+  { Разность множеств }
+  resultSet := subSet(set1, set2); { Разность set1 - set2 }
+  writeln('Элементы множества после разности set1 - set2:');
+  for i := 0 to 500 do
+    if inSet(resultSet, i) then
+      write(i, ' ');
+  writeln;
+
+  { Симметрическая разность }
+  resultSet := symDiffSet(set1, set2); { Симметрическая разность set1 и set2 }
+  writeln('Элементы множества после симметрической разности:');
+  for i := 0 to 500 do
+    if inSet(resultSet, i) then
+      write(i, ' ');
+  writeln;
+
+  { Уничтожение множества }
+  destroySet(set1);
+  destroySet(set2);
+  destroySet(resultSet);
+
+  writeln('Множества уничтожены.');
+end.
