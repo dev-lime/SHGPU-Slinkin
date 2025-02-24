a1192111ba5c47b50e863fb1db045799063ac4f9
LIME <artemdedyuhin@yandex.ru>
2025-02-24 11:14:04 +0500

    some completed 1.2.c task

--- ProductionWorkshop/lab1/1.2.c
+++ ProductionWorkshop/lab1/1.2.c
@@ -1,72 +1,131 @@
 #include <stdio.h>
 #include <limits.h>
- 
-int main()
-{
-    char x, y;
 
-    printf("Unsigned x, y:\n");
-    scanf("%hhd %hhd", &x, &y);
-    unsigned char uxp = x, uxm = x, uy = y;
-    
-    printf("Signed x, y:\n");
-    scanf("%hhd %hhd", &x, &y);
-    signed char sxp = x, sxm = x, sy = y;
-    
-    int ucp, ucm, scp, scm,
-		oucp, oucm, oscp, oscm,
-		uc_min = 0,
-		uc_max = UCHAR_MAX,
-		sc_min = SCHAR_MIN,
-		sc_max = SCHAR_MAX;
+int main() {
+    signed char X_signed;
+    signed char Y_signed;
+    unsigned char X_unsigned;
+    unsigned char Y_unsigned;
 
-    for (ucp = 0; uxp < uc_max; ucp++)
-    {
-		uxp+=uy;
-	}
-	printf("ucp, ");
-	ucp++;
-	for (ucm = 0; uxm > uc_min; ucm++)
-    {
-		uxm-=uy;
-	}
-	printf("ucm, ");
-	ucm++;
-	for (scp = 0; sxp < sc_max; scp++)
-    {
-		sxp+=uy;
-	}
-	printf("scp, ");
-	scp++;
-	for (scm = 0; sxm > sc_min; scm++)
-    {
-		sxm-=uy;
-	}
-	printf("scm.");
-	scm++;
-    printf("\nUNSIGNED Y:\nUnsign x++: %d\nUnsign x--: %d\nSign x++: %d\nSign x--: %d\n", ucp, ucm, scp, scm);
-	
-	for (ucp = 0; uxp < uc_max; ucp++)
-    {
-		uxp+=sy;
-	}
-	ucp++;
-	for (ucm = 0; uxm > uc_min; ucm++)
-    {
-		uxm-=sy;
-	}
-	ucm++;
-	for (scp = 0; sxp < sc_max; scp++)
-    {
-		sxp+=sy;
-	}
-	scp++;
-	for (scm = 0; sxm > sc_min; scm++)
-    {
-		sxm-=sy;
-	}
-	scm++;
-    printf("\nSIGNED Y:\nUnsign x++: %d\nUnsign x--: %d\nSign x++: %d\nSign x--: %d\n", ucp, ucm, scp, scm);
-    
+    printf("Signed X: ");
+    scanf("%hhd", &X_signed);
+    printf("Signed Y: ");
+    scanf("%hhd", &Y_signed);
+    printf("Unsigned X: ");
+    scanf("%hhu", &X_unsigned);
+    printf("Unsigned Y: ");
+    scanf("%hhu", &Y_unsigned);
+
+    int operations_add, operations_sub;
+    char original_X;
+
+    original_X = X_signed;
+    operations_add = 0;
+    while (1) {
+        char new_X = X_signed + Y_signed;
+        if ((new_X < X_signed && Y_signed > 0) || (new_X > X_signed && Y_signed < 0)) {
+            break;
+        }
+        X_signed = new_X;
+        operations_add++;
+    }
+	operations_add++;
+    X_signed = original_X;
+    operations_sub = 0;
+    while (1) {
+        char new_X = X_signed - Y_signed;
+        if ((new_X > X_signed && Y_signed > 0) || (new_X < X_signed && Y_signed < 0)) {
+            break;
+        }
+        X_signed = new_X;
+        operations_sub++;
+    }
+	operations_sub++;
+    printf("\nX is signed, Y is signed\n");
+    printf("Operations X = X + Y: %d\n", operations_add);
+    printf("Operations X = X - Y: %d\n", operations_sub);
+    printf("\n");
+
+    /*original_X = X_signed;
+    operations_add = 0;
+    while (1) {
+        char new_X = X_signed + Y_unsigned;
+        if (new_X < X_signed) {
+            break;
+        }
+        X_signed = new_X;
+        operations_add++;
+    }
+	operations_add++;
+    X_signed = original_X;
+    operations_sub = 0;
+    while (1) {
+        char new_X = X_signed - Y_unsigned;
+        if (new_X > X_signed) {
+            break;
+        }
+        X_signed = new_X;
+        operations_sub++;
+    }
+	operations_sub++;
+    printf("X is signed, Y is unsigned\n");
+    printf("Operations X = X + Y: %d\n", operations_add);
+    printf("Operations X = X - Y: %d\n", operations_sub);
+    printf("\n");*/
+
+    original_X = X_unsigned;
+    operations_add = 0;
+    while (1) {
+        unsigned char new_X = X_unsigned + Y_signed;
+        if ((new_X < X_unsigned && Y_signed > 0) || (new_X > X_unsigned && Y_signed < 0)) {
+            break;
+        }
+        X_unsigned = new_X;
+        operations_add++;
+    }
+	operations_add++;
+    X_unsigned = original_X;
+    operations_sub = 0;
+    while (1) {
+        unsigned char new_X = X_unsigned - Y_signed;
+        if ((new_X > X_unsigned && Y_signed > 0) || (new_X < X_unsigned && Y_signed < 0)) {
+            break;
+        }
+        X_unsigned = new_X;
+        operations_sub++;
+    }
+	operations_sub++;
+    printf("X is unsigned, Y is signed\n");
+    printf("Operations X = X + Y: %d\n", operations_add);
+    printf("Operations X = X - Y: %d\n", operations_sub);
+    printf("\n");
+
+    original_X = X_unsigned;
+    operations_add = 0;
+    while (1) {
+        unsigned char new_X = X_unsigned + Y_unsigned;
+        if (new_X < X_unsigned) {
+            break;
+        }
+        X_unsigned = new_X;
+        operations_add++;
+    }
+	operations_add++;
+    X_unsigned = original_X;
+    operations_sub = 0;
+    while (1) {
+        unsigned char new_X = X_unsigned - Y_unsigned;
+        if (new_X > X_unsigned) {
+            break;
+        }
+        X_unsigned = new_X;
+        operations_sub++;
+    }
+	operations_sub++;
+    printf("X is unsigned, Y is unsigned\n");
+    printf("Operations X = X + Y: %d\n", operations_add);
+    printf("Operations X = X - Y: %d\n", operations_sub);
+    printf("\n");
+
     return 0;
 }
