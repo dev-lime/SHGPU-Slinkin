5615ea97b797b0147ad24233ec5e3a1c3dd39fd5
LIME <artemdedyuhin@yandex.ru>
2025-02-24 10:30:14 +0500

    added gitignore

--- ProductionWorkshop/lab1/1.2.c
+++ ProductionWorkshop/lab1/1.2.c
@@ -3,7 +3,6 @@
  
 int main()
 {
-	// 4 ввода вместо двух, два для signed (x,y) два для unsign (x,y)
     char x, y;
 
     printf("Unsigned x, y:\n");
@@ -14,46 +13,55 @@ int main()
     scanf("%hhd %hhd", &x, &y);
     signed char sxp = x, sxm = x, sy = y;
     
-    int ucp, ucm, scp, scm;
-    
-    for (ucp = 0; uxp < UCHAR_MAX; ucp++)
+    int ucp, ucm, scp, scm,
+		oucp, oucm, oscp, oscm,
+		uc_min = 0,
+		uc_max = UCHAR_MAX,
+		sc_min = SCHAR_MIN,
+		sc_max = SCHAR_MAX;
+
+    for (ucp = 0; uxp < uc_max; ucp++)
     {
 		uxp+=uy;
 	}
+	printf("ucp, ");
 	ucp++;
-	for (ucm = 0; uxm > 0; ucm++)
+	for (ucm = 0; uxm > uc_min; ucm++)
     {
 		uxm-=uy;
 	}
+	printf("ucm, ");
 	ucm++;
-	for (scp = 0; sxp < SCHAR_MAX; scp++)
+	for (scp = 0; sxp < sc_max; scp++)
     {
 		sxp+=uy;
 	}
+	printf("scp, ");
 	scp++;
-	for (scm = 0; sxm > SCHAR_MIN; scm++)
+	for (scm = 0; sxm > sc_min; scm++)
     {
 		sxm-=uy;
 	}
+	printf("scm.");
 	scm++;
     printf("\nUNSIGNED Y:\nUnsign x++: %d\nUnsign x--: %d\nSign x++: %d\nSign x--: %d\n", ucp, ucm, scp, scm);
 	
-	for (ucp = 0; uxp < UCHAR_MAX; ucp++)
+	for (ucp = 0; uxp < uc_max; ucp++)
     {
 		uxp+=sy;
 	}
 	ucp++;
-	for (ucm = 0; uxm > 0; ucm++)
+	for (ucm = 0; uxm > uc_min; ucm++)
     {
 		uxm-=sy;
 	}
 	ucm++;
-	for (scp = 0; sxp < SCHAR_MAX; scp++)
+	for (scp = 0; sxp < sc_max; scp++)
     {
 		sxp+=sy;
 	}
 	scp++;
-	for (scm = 0; sxm > SCHAR_MIN; scm++)
+	for (scm = 0; sxm > sc_min; scm++)
     {
 		sxm-=sy;
 	}
