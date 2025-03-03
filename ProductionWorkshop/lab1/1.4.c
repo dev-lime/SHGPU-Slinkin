83e350e02c88bcb16528205fc821fb057a6a4b94
LIME <artemdedyuhin@yandex.ru>
2025-03-03 10:13:13 +0500

    answer for 1.4 task

--- ProductionWorkshop/lab1/1.4.c
+++ ProductionWorkshop/lab1/1.4.c
@@ -9,3 +9,38 @@
 *   *   *
 */
 
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("N: ");
+    scanf("%d", &N);
+
+    int prev, current;
+    int res = 0;
+    int phase = 0; // 0 - возрастание, 1 - убывание
+    int count = 1;
+
+    printf("Elements:\n");
+    scanf("%d", &prev);
+
+    for (int i = 1; i < N; i++) {
+        scanf("%d", &current);
+        
+        int expected_phase = (count / 3) % 2;
+        int is_increasing = current > prev;
+        int is_decreasing = current < prev;
+        
+        res |= ((expected_phase == 0 && !is_increasing) || (expected_phase == 1 && !is_decreasing));
+
+        int valid_transition = (is_increasing && phase == 0) || (is_decreasing && phase == 1);
+        count = (valid_transition * (count + 1)) + (!valid_transition * 1);
+
+        phase = expected_phase;
+        prev = current;
+    }
+
+    printf("Последовательность %sявляется верной.\n", res == 0 ? "" : "не ");
+
+    return 0;
+}
