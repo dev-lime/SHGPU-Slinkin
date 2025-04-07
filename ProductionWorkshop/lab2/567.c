7cfc59e75121e7cc102b8f1874b3f33b68ba5009
l1me <102348214+dev-lime@users.noreply.github.com>
2025-04-07 08:54:40 +0500

    567 in progress

--- ProductionWorkshop/lab2/567.c
+++ ProductionWorkshop/lab2/567.c
@@ -26,3 +26,38 @@ int main()
     printf("No\n");
     return 0;
 }
+
+/*
+#include <stdio.h>
+
+long long factorial(int n)
+{
+    long long f = 1;
+    for (int i = 2; i <= n; i++)
+        f *= i;
+    return f;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    long long f = 1;
+    for (int i = 2; i <= n; i++)
+        f *= i;
+    long long fact = f;
+    
+    for (long long i = 1; i * (i + 1) * (i + 2) <= fact; i++)
+    {
+        if (i * (i + 1) * (i + 2) == fact)
+        {
+            printf("Yes: %lld * %lld * %lld\n", i, i+1, i+2);
+            return 0;
+        }
+    }
+
+    printf("No\n");
+    return 0;
+}
+*/
