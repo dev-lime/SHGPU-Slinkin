2bd6f36f96c4a7f63d25c08c288ebbab3d40990b
l1me <102348214+dev-lime@users.noreply.github.com>
2025-04-09 08:16:53 +0500

    comleted 567 task

--- ProductionWorkshop/lab2/567.c
+++ ProductionWorkshop/lab2/567.c
@@ -1,52 +1,13 @@
 #include <stdio.h>
 
-long long factorial(int n)
-{
-    long long f = 1;
-    for (int i = 2; i <= n; i++)
-        f *= i;
-    return f;
-}
-
 int main()
 {
     int n;
     scanf("%d", &n);
     
-    long long fact = factorial(n);
-    for (long long i = 1; i * (i + 1) * (i + 2) <= fact; i++)
-    {
-        if (i * (i + 1) * (i + 2) == fact)
-        {
-            printf("Yes: %lld * %lld * %lld\n", i, i+1, i+2);
-            return 0;
-        }
-    }
-
-    printf("No\n");
-    return 0;
-}
-
-/*
-#include <stdio.h>
-
-long long factorial(int n)
-{
-    long long f = 1;
+    long long fact = 1;
     for (int i = 2; i <= n; i++)
-        f *= i;
-    return f;
-}
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    
-    long long f = 1;
-    for (int i = 2; i <= n; i++)
-        f *= i;
-    long long fact = f;
+        fact *= i;
     
     for (long long i = 1; i * (i + 1) * (i + 2) <= fact; i++)
     {
@@ -60,4 +21,3 @@ int main()
     printf("No\n");
     return 0;
 }
-*/
