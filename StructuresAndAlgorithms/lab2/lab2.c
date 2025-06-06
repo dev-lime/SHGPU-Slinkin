ec4f98e9e721d61aa4a79e387b74edcfcb93b918
l1me <102348214+dev-lime@users.noreply.github.com>
2025-06-06 08:34:33 +0500

    Some edited in Programming

--- StructuresAndAlgorithms/lab2/lab2.c
+++ StructuresAndAlgorithms/lab2/lab2.c
@@ -1,11 +1,10 @@
-#include "findunit.h"
+#include <stdio.h>
 
 int find_count = 0;
 
-int line_find_one(const int src[], int src_size, testfunc func)
+int line_find_one(const int src[], int src_size, int (*func)(int))
 {
 	find_count = 0;
-
 	for (int i = 0; i < src_size; i++)
 	{
 		find_count++;
@@ -14,29 +13,26 @@ int line_find_one(const int src[], int src_size, testfunc func)
 			return i;
 		}
 	}
-
 	return -1;
 }
 
-int line_find_all(const int src[], int src_size, testfunc func,
+int line_find_all(const int src[], int src_size, int (*func)(int),
 				  int result[], int result_maxsize)
 {
 	find_count = 0;
-	int found = 0;
-
-	for (int i = 0; i < src_size && found < result_maxsize; i++)
+	int count = 0;
+	for (int i = 0; i < src_size && count < result_maxsize; i++)
 	{
 		find_count++;
 		if (func(src[i]) == 1)
 		{
-			result[found++] = i;
+			result[count++] = i;
 		}
 	}
-
-	return found;
+	return count;
 }
 
-int bin_find_one(const int src[], int src_size, testfunc func)
+int bin_find_one(const int src[], int src_size, int (*func)(int))
 {
 	find_count = 0;
 	int left = 0;
@@ -61,35 +57,30 @@ int bin_find_one(const int src[], int src_size, testfunc func)
 			left = mid + 1;
 		}
 	}
-
 	return -1;
 }
 
-int bin_find_all(const int src[], int src_size, testfunc func,
+int bin_find_all(const int src[], int src_size, int (*func)(int),
 				 int *res_beg, int *res_end)
 {
 	find_count = 0;
 
-	// First find one occurrence
 	int pos = bin_find_one(src, src_size, func);
 	if (pos == -1)
 	{
-		*res_beg = -1;
-		*res_end = -1;
 		return 0;
 	}
 
-	// Search for the beginning of the range
 	int left = 0;
 	int right = pos;
-	int beg = pos;
+	int start = pos;
 	while (left <= right)
 	{
 		find_count++;
 		int mid = left + (right - left) / 2;
 		if (func(src[mid]) == 0)
 		{
-			beg = mid;
+			start = mid;
 			right = mid - 1;
 		}
 		else
@@ -98,7 +89,6 @@ int bin_find_all(const int src[], int src_size, testfunc func,
 		}
 	}
 
-	// Search for the end of the range
 	left = pos;
 	right = src_size - 1;
 	int end = pos;
@@ -117,7 +107,50 @@ int bin_find_all(const int src[], int src_size, testfunc func,
 		}
 	}
 
-	*res_beg = beg;
+	*res_beg = start;
 	*res_end = end;
-	return end - beg + 1;
+	return end - start + 1;
+}
+
+int test_equal(int target, int value)
+{
+	return (value == target) ? 0 : (value < target ? -1 : 1);
+}
+
+int test_greater(int value)
+{
+	return (value > 50) ? 1 : 0;
+}
+
+int main()
+{
+	int arr[] = {10, 20, 30, 40, 50, 50, 50, 60, 70, 80, 90, 100};
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Linear search for first element > 50:\n");
+	int index = line_find_one(arr, size, test_greater);
+	printf("Found at index: %d, iterations: %d\n", index, find_count);
+
+	printf("\nLinear search for all elements > 50:\n");
+	int results[10];
+	int count = line_find_all(arr, size, test_greater, results, 10);
+	printf("Found %d elements: ", count);
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d ", results[i]);
+	}
+	printf(", iterations: %d\n", find_count);
+
+	printf("\nBinary search for 50:\n");
+	int (*test_equal_50)(int) = &test_equal;
+	index = bin_find_one(arr, size, test_equal_50);
+	printf("Found at index: %d, iterations: %d\n", index, find_count);
+
+	printf("\nBinary search for all 50s:\n");
+	int beg, end;
+	count = bin_find_all(arr, size, test_equal_50, &beg, &end);
+	printf("Found %d elements from index %d to %d, iterations: %d\n",
+		   count, beg, end, find_count);
+
+	return 0;
 }
