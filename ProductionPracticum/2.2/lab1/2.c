311c57a133cb9eb6609e20c53e028635d8ed6d43
l1me <102348214+dev-lime@users.noreply.github.com>
2025-10-15 14:04:01 +0500

    Refactoring

---
+++ ProductionPracticum/2.2/lab1/2.c
@@ -0,0 +1,145 @@
+/*
+Построение "ковра Серпинского".
+Дано:
+в первой строке одно натуральное значение - исходный размер стороны "ковра Серпинского",
+во второй - количество итераций для получения результирующего фрактала,
+в третьей - цвет исходного квадрата в формате RGB,
+в четвертой - цвет изымаемых квадратов в формате RGB.
+
+Найти:
+сформировать "ковер Серпинского"
+
+Требования и рекомендации:
+Для решения задачи следует сформировать двумерный массив squares из необходимого количества структур
+типа square_t (координаты левого верхнего угла и длина стороны), описывающих квадрат,
+который будет изыматься из центров исходных квадратов на каждой итерации.
+Рекомендуется использовать рекурсивное решение. Для хранения изображения следует использовать матрицу image,
+элементы которой представляют собой структуру RGB для хранения цвета каждой точки.
+Обработка сформированного массива squares должна обеспечить заполнение массива image.
+
+Результат:
+вывод полученного изображения из массива image на стандартный поток вывода в формате ppm P3.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct
+{
+	unsigned char r, g, b;
+} RGB;
+
+typedef struct
+{
+	int x, y; // левый верхний угол
+	int size; // длина стороны
+} square_t;
+
+void generate_sierpinski(square_t **squares, int *count, int x, int y, int size, int iterations, int current_iteration)
+{
+	if (current_iteration >= iterations)
+	{
+		return;
+	}
+
+	int new_size = size / 3;
+	int new_x = x + new_size;
+	int new_y = y + new_size;
+
+	(*count)++;
+	*squares = (square_t *)realloc(*squares, *count * sizeof(square_t));
+	(*squares)[*count - 1] = (square_t){new_x, new_y, new_size};
+
+	// Рекурсивно обрабатывает 8 оставшихся квадратов
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (i == 1 && j == 1)
+				continue; // пропускает центральный квадрат
+
+			int next_x = x + i * new_size;
+			int next_y = y + j * new_size;
+			generate_sierpinski(squares, count, next_x, next_y, new_size, iterations, current_iteration + 1);
+		}
+	}
+}
+
+void draw_squares(RGB **image, int image_size, square_t *squares, int squares_count, RGB base_color, RGB remove_color)
+{
+	for (int y = 0; y < image_size; y++)
+	{
+		for (int x = 0; x < image_size; x++)
+		{
+			image[y][x] = base_color;
+		}
+	}
+
+	for (int i = 0; i < squares_count; i++)
+	{
+		square_t sq = squares[i];
+		for (int y = sq.y; y < sq.y + sq.size && y < image_size; y++)
+		{
+			for (int x = sq.x; x < sq.x + sq.size && x < image_size; x++)
+			{
+				if (x >= 0 && y >= 0)
+				{
+					image[y][x] = remove_color;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int initial_size, iterations;
+	RGB base_color, remove_color;
+
+	scanf("%d", &initial_size);
+	scanf("%d", &iterations);
+	scanf("%hhu %hhu %hhu", &base_color.r, &base_color.g, &base_color.b);
+	scanf("%hhu %hhu %hhu", &remove_color.r, &remove_color.g, &remove_color.b);
+
+	int image_size = initial_size;
+
+	RGB **image = (RGB **)malloc(image_size * sizeof(RGB *));
+	for (int i = 0; i < image_size; i++)
+	{
+		image[i] = (RGB *)malloc(image_size * sizeof(RGB));
+		for (int j = 0; j < image_size; j++)
+		{
+			image[i][j] = base_color; // заполняет базовым цветом
+		}
+	}
+
+	// Генерирует квадраты для удаления
+	square_t *squares = NULL;
+	int squares_count = 0;
+
+	generate_sierpinski(&squares, &squares_count, 0, 0, initial_size, iterations, 0);
+	draw_squares(image, image_size, squares, squares_count, base_color, remove_color);
+
+	printf("P3\n");
+	printf("%d %d\n", image_size, image_size);
+	printf("255\n");
+
+	for (int y = 0; y < image_size; y++)
+	{
+		for (int x = 0; x < image_size; x++)
+		{
+			printf("%d %d %d ", image[y][x].r, image[y][x].g, image[y][x].b);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < image_size; i++)
+	{
+		free(image[i]);
+	}
+	free(image);
+	free(squares);
+
+	return 0;
+}
---
+++ ProductionPracticum/2.2/lab3/lab3.c
@@ -0,0 +1,218 @@
+/*
+Создать утилиту cml (упрощенный аналог утилит cp, mv, ln)для копирования/перемещения
+регулярных файлов и создания ссылок на регулярные файлы. Общий вид:
+cml [-h|-l|-m] исходный_файл результирующий_файл
+Поведение утилиты:
+1) Если синтаксис вызова утилиты неверен (отсутствуют один или оба обязательных параметра,
+обязательных параметров больше двух, используются несуществующие опции) - сообщить об этом,
+вывести краткую справку и остановить программу.
+Примеры верного синтакиса:
+cml -h 1 2
+cml 123 456.txt
+cml -h -l -x
+cml -h -l
+Примеры НЕверного синтакиса:
+cml -k 123 456.txt
+cml 1 2 3 4
+cml
+cml -l
+2) Если исходный файл не существует - сообщить об этом и остановить программу.
+3) Если исходный файл существует, но не является регулярным файлом или ссылкой - сообщить об этом и остановить программу.
+4) Если результирующий файл существует, но не является регулярным файлом или ссылкой - сообщить об этом и остановить программу.
+5) Если результирующий файл не существует, то утилита либо копирует исходный файл (если опция отсутствует),
+либо перемещает/переименовывает исходный файл (-m), либо создает жесткую ссылку (-h), либо создает символическую ссылку (-l)
+6) Если результирующий файл существует и является регулярным файлом или ссылкой,
+следует запросить у пользователя разрешение на удаление.
+При положительном ответе - удалить файл и далее работать по предыдущему варианту.
+При отрицательном - остановить программу с соответствующим сообщением.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 4096
+
+void print_help()
+{
+	printf("Использование: cml [-h|-l|-m] исходный_файл целевой_файл\n");
+	printf("Опции:\n");
+	printf("  -h  Создать жесткую ссылку\n");
+	printf("  -l  Создать символическую ссылку\n");
+	printf("  -m  Переместить/переименовать файл\n");
+	printf("Если опция не указана, файл будет скопирован.\n");
+}
+
+int is_regular_file(const char *path)
+{
+	struct stat path_stat;
+	if (stat(path, &path_stat) != 0)
+	{
+		return 0;
+	}
+	return S_ISREG(path_stat.st_mode) || S_ISLNK(path_stat.st_mode);
+}
+
+int copy_file(const char *src, const char *dst)
+{
+	int src_fd, dst_fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	src_fd = open(src, O_RDONLY);
+	if (src_fd == -1)
+	{
+		perror("Ошибка открытия исходного файла");
+		return 0;
+	}
+
+	dst_fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (dst_fd == -1)
+	{
+		perror("Ошибка открытия целевого файла");
+		close(src_fd);
+		return 0;
+	}
+
+	while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(dst_fd, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			perror("Ошибка записи в целевой файл");
+			close(src_fd);
+			close(dst_fd);
+			return 0;
+		}
+	}
+
+	close(src_fd);
+	close(dst_fd);
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	int h_flag = 0, l_flag = 0, m_flag = 0;
+	char *source_file = NULL;
+	char *target_file = NULL;
+
+	// Проверка количества аргументов
+	if (argc < 3 || argc > 4)
+	{
+		fprintf(stderr, "Ошибка: Неверное количество аргументов\n");
+		print_help();
+		return 1;
+	}
+
+	// Проверка опций
+	if (argc == 4)
+	{
+		if (strcmp(argv[1], "-h") == 0)
+		{
+			h_flag = 1;
+		}
+		else if (strcmp(argv[1], "-l") == 0)
+		{
+			l_flag = 1;
+		}
+		else if (strcmp(argv[1], "-m") == 0)
+		{
+			m_flag = 1;
+		}
+		else
+		{
+			fprintf(stderr, "Ошибка: Неизвестная опция '%s'\n", argv[1]);
+			print_help();
+			return 1;
+		}
+
+		source_file = argv[2];
+		target_file = argv[3];
+	}
+	else
+	{
+		source_file = argv[1];
+		target_file = argv[2];
+	}
+
+	// Проверка существования исходного файла и то что это обычный файл или ссылка
+	if (!is_regular_file(source_file))
+	{
+		fprintf(stderr, "Ошибка: Исходный файл '%s' не существует или не является обычным файлом/ссылкой\n", source_file);
+		return 1;
+	}
+
+	// Проверка существования целевого файла
+	struct stat target_stat;
+	int target_exists = (stat(target_file, &target_stat) == 0);
+
+	if (target_exists)
+	{
+		// Проверка что цель - обычный файл или ссылка
+		if (!is_regular_file(target_file))
+		{
+			fprintf(stderr, "Ошибка: Целевой файл '%s' существует, но не является обычным файлом/ссылкой\n", target_file);
+			return 1;
+		}
+
+		// Запрос подтверждения у пользователя
+		printf("Целевой файл '%s' существует. Перезаписать? (y/n): ", target_file);
+		char response;
+		scanf(" %c", &response);
+		if (response != 'y' && response != 'Y')
+		{
+			printf("Операция отменена\n");
+			return 0;
+		}
+
+		// Удаление существующего файла
+		if (unlink(target_file) != 0)
+		{
+			perror("Ошибка удаления целевого файла");
+			return 1;
+		}
+	}
+
+	// Выполнение запрошенной операции
+	if (h_flag)
+	{
+		if (link(source_file, target_file) != 0)
+		{
+			perror("Ошибка создания жесткой ссылки");
+			return 1;
+		}
+		printf("Жесткая ссылка создана из '%s' в '%s'\n", source_file, target_file);
+	}
+	else if (l_flag)
+	{
+		if (symlink(source_file, target_file) != 0)
+		{
+			perror("Ошибка создания символической ссылки");
+			return 1;
+		}
+		printf("Символическая ссылка создана из '%s' в '%s'\n", source_file, target_file);
+	}
+	else if (m_flag)
+	{
+		if (rename(source_file, target_file) != 0)
+		{
+			perror("Ошибка перемещения файла");
+			return 1;
+		}
+		printf("Файл перемещен из '%s' в '%s'\n", source_file, target_file);
+	}
+	else
+	{
+		if (!copy_file(source_file, target_file))
+		{
+			return 1;
+		}
+		printf("Файл скопирован из '%s' в '%s'\n", source_file, target_file);
+	}
+
+	return 0;
+}
