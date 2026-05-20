9a08db2fb3d5592dcf3988b36a5536e51121423d
l1me <102348214+dev-lime@users.noreply.github.com>
2026-05-20 18:17:25 +0500

    Fix lab3

--- Programming/3.2/lab3/graf.pas
+++ Programming/3.2/lab3/graf.pas
@@ -6,9 +6,31 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  ColorBox, Spin, uPSComponent, uPSRuntime, uPSCompiler;
+  ColorBox, Spin;
 
 type
+
+  { TFormulaParser }
+
+  TFormulaParser = class
+  private
+    FExpr: string;
+    FPos: Integer;
+    FX: Double;
+    function ParseExpression: Double;
+    function ParseTerm: Double;
+    function ParseFactor: Double;
+    function ParsePower: Double;
+    function ParsePrimary: Double;
+    function ParseFunctionOrVar: Double;
+    function ParseNumber: Double;
+    procedure SkipSpaces;
+    function CurrentChar: Char;
+    function IsDigit(c: Char): Boolean;
+    function IsLetter(c: Char): Boolean;
+  public
+    function Evaluate(const expr: string; x: Double): Double;
+  end;
 
   { TForm1 }
 
@@ -23,27 +45,23 @@ type
     Label3: TLabel;
     Label4: TLabel;
     PaintBox1: TPaintBox;
-    PSScript1: TPSScript;
     procedure Btn_DrawClick(Sender: TObject);
     procedure FormResize(Sender: TObject);
     procedure PaintBox1Paint(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure PSScript1Compile(Sender: TPSScript);
-    procedure PSScript1Execute(Sender: TPSScript);
+    procedure FormDestroy(Sender: TObject);
+    procedure ScaleChanged(Sender: TObject);
+    procedure UpdateFromControls;
   private
     FFormula: string;
     FScaleX: Double;
     FScaleY: Double;
     FGraphColor: TColor;
     FNeedDraw: Boolean;
-    FHasResult: Boolean;
-    FResultValue: Double;
-    FXValue: Double;
+    FParser: TFormulaParser;
     function EvaluateFormula(x: Double): Double;
     procedure DrawGraph;
     procedure DrawAxes;
-    procedure PSScript1ReadVar(Sender: TPSScript; const VarName: string; var Value: Variant; var Success: Boolean);
-    procedure PSScript1WriteVar(Sender: TPSScript; const VarName: string; var Value: Variant; var Success: Boolean);
   public
 
   end;
@@ -57,6 +75,213 @@ implementation
 
 uses Math;
 
+{ TFormulaParser }
+
+function TFormulaParser.CurrentChar: Char;
+begin
+  if FPos <= Length(FExpr) then
+    Result := FExpr[FPos]
+  else
+    Result := #0;
+end;
+
+function TFormulaParser.IsDigit(c: Char): Boolean;
+begin
+  Result := (c >= '0') and (c <= '9');
+end;
+
+function TFormulaParser.IsLetter(c: Char): Boolean;
+begin
+  Result := ((c >= 'a') and (c <= 'z')) or ((c >= 'A') and (c <= 'Z'));
+end;
+
+procedure TFormulaParser.SkipSpaces;
+begin
+  while CurrentChar = ' ' do
+    Inc(FPos);
+end;
+
+function TFormulaParser.ParseNumber: Double;
+var
+  start: Integer;
+  numStr: string;
+  hasDot: Boolean;
+begin
+  SkipSpaces;
+  start := FPos;
+  hasDot := False;
+  while IsDigit(CurrentChar) or (CurrentChar = '.') do
+  begin
+    if CurrentChar = '.' then
+    begin
+      if hasDot then Break;
+      hasDot := True;
+    end;
+    Inc(FPos);
+  end;
+  numStr := Copy(FExpr, start, FPos - start);
+  Result := StrToFloatDef(numStr, 0);
+end;
+
+function TFormulaParser.ParseFunctionOrVar: Double;
+var
+  funcName: string;
+  arg: Double;
+  start: Integer;
+begin
+  SkipSpaces;
+  start := FPos;
+  while IsLetter(CurrentChar) do
+    Inc(FPos);
+  funcName := LowerCase(Copy(FExpr, start, FPos - start));
+
+  if funcName = 'x' then
+  begin
+    Result := FX;
+    Exit;
+  end;
+
+  SkipSpaces;
+  if CurrentChar = '(' then
+  begin
+    Inc(FPos);
+    arg := ParseExpression;
+    SkipSpaces;
+    if CurrentChar = ')' then
+      Inc(FPos);
+  end
+  else
+    arg := 0;
+
+  if funcName = 'sin' then Result := Sin(arg)
+  else if funcName = 'cos' then Result := Cos(arg)
+  else if funcName = 'tan' then Result := Tan(arg)
+  else if funcName = 'exp' then Result := Exp(arg)
+  else if funcName = 'ln' then Result := Ln(arg)
+  else if funcName = 'sqrt' then Result := Sqrt(arg)
+  else if funcName = 'abs' then Result := Abs(arg)
+  else if funcName = 'arctan' then Result := ArcTan(arg)
+  else if funcName = 'sqr' then Result := Sqr(arg)
+  else if funcName = 'pi' then Result := Pi
+  else Result := 0;
+end;
+
+function TFormulaParser.ParsePrimary: Double;
+begin
+  SkipSpaces;
+  if CurrentChar = '(' then
+  begin
+    Inc(FPos);
+    Result := ParseExpression;
+    SkipSpaces;
+    if CurrentChar = ')' then
+      Inc(FPos);
+  end
+  else if IsLetter(CurrentChar) then
+    Result := ParseFunctionOrVar
+  else if IsDigit(CurrentChar) or (CurrentChar = '.') then
+    Result := ParseNumber
+  else
+    Result := 0;
+end;
+
+function TFormulaParser.ParsePower: Double;
+var
+  baseVal, exponent: Double;
+begin
+  Result := ParsePrimary;
+
+  SkipSpaces;
+  if CurrentChar = '^' then
+  begin
+    Inc(FPos);
+    exponent := ParsePower;
+    Result := Power(Result, exponent);
+  end;
+end;
+
+function TFormulaParser.ParseFactor: Double;
+begin
+  SkipSpaces;
+  if CurrentChar = '-' then
+  begin
+    Inc(FPos);
+    Result := -ParseFactor;
+  end
+  else if CurrentChar = '+' then
+  begin
+    Inc(FPos);
+    Result := ParseFactor;
+  end
+  else
+    Result := ParsePower;
+end;
+
+function TFormulaParser.ParseTerm: Double;
+var
+  right: Double;
+begin
+  Result := ParseFactor;
+  while True do
+  begin
+    SkipSpaces;
+    if CurrentChar = '*' then
+    begin
+      Inc(FPos);
+      right := ParseFactor;
+      Result := Result * right;
+    end
+    else if CurrentChar = '/' then
+    begin
+      Inc(FPos);
+      right := ParseFactor;
+      if right <> 0 then
+        Result := Result / right
+      else
+        Result := 0;
+    end
+    else
+      Break;
+  end;
+end;
+
+function TFormulaParser.ParseExpression: Double;
+var
+  right: Double;
+begin
+  Result := ParseTerm;
+  while True do
+  begin
+    SkipSpaces;
+    if CurrentChar = '+' then
+    begin
+      Inc(FPos);
+      right := ParseTerm;
+      Result := Result + right;
+    end
+    else if CurrentChar = '-' then
+    begin
+      Inc(FPos);
+      right := ParseTerm;
+      Result := Result - right;
+    end
+    else
+      Break;
+  end;
+end;
+
+function TFormulaParser.Evaluate(const expr: string; x: Double): Double;
+begin
+  FExpr := expr;
+  FPos := 1;
+  FX := x;
+  try
+    Result := ParseExpression;
+  except
+    Result := 0;
+  end;
+end;
+
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -66,69 +291,35 @@ begin
   FScaleY := FloatSpinEdit2.Value;
   FGraphColor := ColorBox1.Selected;
   FNeedDraw := False;
-  FHasResult := False;
-  FResultValue := 0;
-  FXValue := 0;
+  FParser := TFormulaParser.Create;
 end;
 
-procedure TForm1.PSScript1Compile(Sender: TPSScript);
+procedure TForm1.FormDestroy(Sender: TObject);
 begin
-  Sender.AddFunction(@Sin, 'function sin(x: Double): Double;');
-  Sender.AddFunction(@Cos, 'function cos(x: Double): Double;');
-  Sender.AddFunction(@Tan, 'function tan(x: Double): Double;');
-  Sender.AddFunction(@Exp, 'function exp(x: Double): Double;');
-  Sender.AddFunction(@Ln, 'function ln(x: Double): Double;');
-  Sender.AddFunction(@Sqrt, 'function sqrt(x: Double): Double;');
-  Sender.AddFunction(@Abs, 'function abs(x: Double): Double;');
-  Sender.AddFunction(@ArcTan, 'function arctan(x: Double): Double;');
-  Sender.AddFunction(@Sqr, 'function sqr(x: Double): Double;');
-  Sender.AddFunction(@Pi, 'function pi: Double;');
-  Sender.AddFunction(@Power, 'function power(base: Double; exponent: Double): Double;');
+  FParser.Free;
 end;
 
-procedure TForm1.PSScript1Execute(Sender: TPSScript);
+procedure TForm1.UpdateFromControls;
 begin
+  FFormula := Edit_Formula.Text;
+  FScaleX := FloatSpinEdit1.Value;
+  FScaleY := FloatSpinEdit2.Value;
+  FGraphColor := ColorBox1.Selected;
+  FNeedDraw := True;
 end;
 
-procedure TForm1.PSScript1ReadVar(Sender: TPSScript; const VarName: string; var Value: Variant; var Success: Boolean);
+procedure TForm1.ScaleChanged(Sender: TObject);
 begin
-  if VarName = 'x' then
-  begin
-    Value := FXValue;
-    Success := True;
-  end;
-end;
-
-procedure TForm1.PSScript1WriteVar(Sender: TPSScript; const VarName: string; var Value: Variant; var Success: Boolean);
-begin
-  if VarName = 'Result' then
-  begin
-    FResultValue := Value;
-    FHasResult := True;
-    Success := True;
-  end;
+  UpdateFromControls;
+  PaintBox1.Repaint;
 end;
 
 function TForm1.EvaluateFormula(x: Double): Double;
-var
-  scriptCode: string;
 begin
-  Result := 0;
-  FHasResult := False;
-  FResultValue := 0;
   try
-    FXValue := x;
-    scriptCode := 'var x, Result: Double; begin Result := ' + FFormula + '; end;';
-    PSScript1.Script.Text := scriptCode;
-    if PSScript1.Compile then
-    begin
-      PSScript1.Execute;
-      if FHasResult then
-        Result := FResultValue;
-    end;
+    Result := FParser.Evaluate(FFormula, x);
   except
     Result := 0;
-    FHasResult := False;
   end;
 end;
 
@@ -146,8 +337,8 @@ begin
   PaintBox1.Canvas.Pen.Color := clSilver;
   PaintBox1.Canvas.Pen.Width := 1;
 
-  gridStep := Round(50 * FScaleX);
-  if gridStep < 20 then gridStep := 50;
+  gridStep := Round(FScaleX);
+  if gridStep < 20 then gridStep := 20;
 
   i := cx mod gridStep;
   while i <= w do
@@ -201,8 +392,8 @@ begin
 
   if FFormula = '' then Exit;
 
-  xStart := -cx / (50 * FScaleX);
-  xEnd := cx / (50 * FScaleX);
+  xStart := -cx / FScaleX;
+  xEnd := cx / FScaleX;
   dx := (xEnd - xStart) / w;
 
   PaintBox1.Canvas.Pen.Color := FGraphColor;
@@ -224,8 +415,8 @@ begin
       Continue;
     end;
 
-    px := cx + Round(x * 50 * FScaleX);
-    py := cy - Round(val * 50 * FScaleY);
+    px := cx + Round(x * FScaleX);
+    py := cy - Round(val * FScaleY);
 
     if py < -10000 then py := -10000;
     if py > h + 10000 then py := h + 10000;
@@ -251,11 +442,7 @@ end;
 
 procedure TForm1.Btn_DrawClick(Sender: TObject);
 begin
-  FFormula := Edit_Formula.Text;
-  FScaleX := FloatSpinEdit1.Value;
-  FScaleY := FloatSpinEdit2.Value;
-  FGraphColor := ColorBox1.Selected;
-  FNeedDraw := True;
+  UpdateFromControls;
   PaintBox1.Repaint;
 end;
 
--- Programming/3.2/lab3/lab3.lpr
+++ Programming/3.2/lab3/lab3.lpr
@@ -10,7 +10,7 @@ uses
   athreads,
   {$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, pascalscript, graf
+  Forms, graf
   { you can add units after this };
 
 {$R *.res}
