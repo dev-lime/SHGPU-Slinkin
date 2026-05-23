017bfdf3104ae901d50a84c601261e6c3cbb0adb
l1me <102348214+dev-lime@users.noreply.github.com>
2026-05-23 12:54:54 +0500

    Create lab4

---
+++ Programming/3.2/lab4/backup/gred.pas
@@ -0,0 +1,31 @@
+unit gred;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    PaintBox1: TPaintBox;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Programming/3.2/lab4/gred.pas
@@ -0,0 +1,33 @@
+unit gred;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls,
+  ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    PaintBox1: TPaintBox;
+    StatusBar1: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
